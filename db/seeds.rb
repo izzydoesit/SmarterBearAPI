48109cd927e1188646a00222d81938e61e9d6067
Sam Parker <samuel.gage.parker@gmail.com>
2016-10-14 14:14:28 -0700

    add json files with query data from edgar-online

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Example Edgar-Online query string:
+# http://edgaronline.api.mashery.com/v2/insiders/transactions?fields=%21filerCity%2C%21filerStateid%2C%21filerState%2C%21filerZip%2C%21filerPhone%2C%21issueCity%2C%21issueStateid%2C%21issueState%2C%21issueZip&issuenames=%2A[COMPANY NAME]%2A&debug=true&sortby=filerid+asc&appkey=[API KEY]
