ba0f48acfa842112ba1efaa8aea0e4ebe1c521e1
Sam Parker <samuel.gage.parker@gmail.com>
2016-10-16 11:43:48 -0700

    correct error for insiders route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :forms, only: ['index']
   resources :companies, only: ['index']
-  resources :transactions, only: ['index']
+  resources :insiders, only: ['index']
 
   root 'companies#index'
 end
