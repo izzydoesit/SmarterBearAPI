66988d9c5019c06284140aacd062bb44d14ecb19
Sam Parker <samuel.gage.parker@gmail.com>
2016-10-17 11:52:39 -0700

    add search function to company resource

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,9 +5,21 @@ class Company < ApplicationRecord
   has_many :transactions, through: :insiders
 
   validates :name, :cik_number, :ticker, presence: true
-
-  # need to add in shares outstanding once data is found
+ 
+  # need to add in shares outstanding once data is found 
   def rate_confidence
     self.transactions_this_month.count
+  end
+
+  def self.search_by_name(name)
+    self.where("name like ?", "#{name.downcase.capitalize[0..3]}%")
+  end
+
+  def self.search_by_ticker(ticker)
+    self.where("ticker like ?", "#{ticker.upcase[0..3]}%")
+  end
+
+  def self.search(param)
+    [search_by_name(param), search_by_ticker(param)].flatten.uniq
   end
 end
