de43573a21f0e846dba13b329b18dc2577ef4a3b
Sam Parker <samuel.gage.parker@gmail.com>
2016-10-16 17:03:24 -0700

    adjust company model validations

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,7 +2,7 @@ class Company < ApplicationRecord
   has_many :insiders
   has_many :transactions, through: :insiders
 
-  validates :name, presence: true
+  validates :name, :cik_number, :ticker presence: true
 
-  # need to add in shares outstanding and ticker validation once data is found
+  # need to add in shares outstanding once data is found
 end
