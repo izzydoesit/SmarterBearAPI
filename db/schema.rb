1a98621a50941e08350226e85865119c9c08bd84
Izzy Matos <israeldmatos@gmail.com>
2016-10-18 15:51:29 -0700

    Add insider_score column to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161016000144) do
+ActiveRecord::Schema.define(version: 20161018223252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,8 +39,9 @@ ActiveRecord::Schema.define(version: 20161016000144) do
     t.string   "name"
     t.string   "relationship"
     t.integer  "company_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "insider_score"
     t.index ["company_id"], name: "index_insiders_on_company_id", using: :btree
   end
 
